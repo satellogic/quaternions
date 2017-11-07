2c7c7a6caf3980d728c978abe3a40e0ba2bcc24a
slava <slava@satellogic.com>
2017-11-08 00:36:14 +0200

    update for pypi

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
-    name='quaternions',
+    name='satellogic_quaternions',
     version='0.1.3',
     author='Matias Graña',
     author_email='matias@satellogic.com',
     long_description='This is a library for dealing with quaternions in python in a unified way.',
-    packages=find_packages(exclude=["tests"]),
-    license="?",
+    packages=['satellogic_quaternions'],
+    license="Satellogic SA Copyright 2017. All our code is GPLv3 licensed.",
     classifiers=[
         'Development Status :: 1 - Beta',
         'Environment :: Console',
@@ -21,5 +21,6 @@ setup(
     ],
     install_requires=[
         'numpy',
+        'hypothesis'
     ],
 )
