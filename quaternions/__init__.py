16d0b12ccf462990d56aae63764322d08c39153e
slava <slava@satellogic.com>
2017-11-22 10:19:02 +0200

    rename Quaternion

--- quaternions/__init__.py
+++ quaternions/__init__.py
@@ -1,3 +1,3 @@
-from quaternions.quaternions import Quaternion  # NOQA
+from quaternions.quaternion import Quaternion  # NOQA
 from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
 from quaternions.version import __version__  # NOQA
