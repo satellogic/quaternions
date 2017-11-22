fix Quaternion.distance

quaternions/quaternion.py
@@ -51,8 +51,9 @@ class Quaternion(GeneralQuaternion):
         return self.is_equal(other)
 
     def distance(self, other):
-        """ Returns the distance in radians between two unitary quaternions. """
-        return min(super().euclidean_distance(other), super().euclidean_distance(-other))
+        """ Returns the distance [rad] between two unitary quaternions. """
+        diff = self * ~other
+        return diff.rotation_angle()
 
     @property
     def positive_representant(self):
@@ -127,8 +128,10 @@ class Quaternion(GeneralQuaternion):
         return v / np.linalg.norm(v)
 
     def rotation_angle(self):
-        """ returns rotation angle [rad] """
-        return np.linalg.norm(self.rotation_vector)
+        """ returns rotation angle [rad], in [0, 2 * pi) """
+        angle = np.linalg.norm(self.rotation_vector)
+        angle %= 2 * np.math.pi
+        return angle
 
     @property
     def ra_dec_roll(self):

tests/test_quaternion.py
@@ -40,12 +40,17 @@ class QuaternionTest(unittest.TestCase):
         assert q != q + Quaternion(1, 2, 3, 4)
         assert q == GeneralQuaternion(*q.coordinates)
 
-    @given(ANY_QUATERNION)
-    def test_distance(self, arr):
+    @given(ANY_QUATERNION, strategies.floats(min_value=0, max_value=2 * np.math.pi-1e-4))
+    def test_distance(self, arr, angle_rad):
         assume(GeneralQuaternion(*arr).norm() > DEFAULT_TOLERANCE)
         q = Quaternion(*arr)
-        assert q.distance(q) == pytest.approx(0)
-        assert q.distance(-q) == pytest.approx(0)
+        assert q.distance(-q) == pytest.approx(0) or q.distance(-q) == pytest.approx(2 * np.math.pi)
+
+        for diff in [Quaternion.from_ra_dec_roll(np.degrees(angle_rad), 0, 0),
+                     Quaternion.from_ra_dec_roll(0, np.degrees(angle_rad), 0),
+                     Quaternion.from_ra_dec_roll(0, 0, np.degrees(angle_rad))]:
+            assert q.distance(q * diff) == pytest.approx(angle_rad)
+            assert q.distance(diff * q) == pytest.approx(angle_rad)
 
     @given(ANY_QUATERNION)
     def test_positive_representant(self, arr):