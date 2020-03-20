120fe567aadf5d0cd62d8438e673c76ccbe384db
Juan Luis Cano Rodríguez <juanlu@satellogic.com>
2020-03-20 14:47:09 +0100

    Change import order

--- quaternions/__init__.py
+++ quaternions/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from quaternions.quaternion import Quaternion  # NOQA
 from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
 
@@ -5,5 +7,4 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-import warnings
 warnings.simplefilter('once')
