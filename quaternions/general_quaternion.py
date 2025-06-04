7e0096f383593e7a31dd8cc70b92922f59e19da7
Giorgio Salluzzo <giorgio.salluzzo@satellogic.com>
2025-06-04 19:05:46 +0200

    Fix for test.

--- quaternions/general_quaternion.py
+++ quaternions/general_quaternion.py
@@ -77,11 +77,12 @@ class GeneralQuaternion(object):
         coordinates = []
         for _c in self.coordinates:
             try:
+                # convert a Numpy obj to its Python counterpart
                 c = _c.item()
             except AttributeError:
                 c = _c
             coordinates.append(c)
-        return '{}{}'.format(self.__class__.__name__, tuple(self.coordinates))
+        return '{}{}'.format(self.__class__.__name__, tuple(coordinates))
 
     def __str__(self):
         return '({qr:.6g}{qi:+.6g}i{qj:+.6g}j{qk:+.6g}k)'.format(**self.__dict__)
