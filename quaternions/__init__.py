3047d49333ab7a49369997eff8436b57b9d9e6d6
Giorgio Salluzzo <giorgio.salluzzo@satellogic.com>
2025-06-06 18:50:46 +0200

    Fix.

--- quaternions/__init__.py
+++ quaternions/__init__.py
@@ -1,13 +1,16 @@
 import warnings
 
 try:
-    from importlib.metadata import version
+    from importlib.metadata import version, PackageNotFoundError
 except ImportError:
-    from importlib_metadata import version
+    from importlib_metadata import version, PackageNotFoundError  # for Python < 3.8
 
 from quaternions.quaternion import Quaternion  # NOQA
 from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
 
 warnings.simplefilter('once')
 
-__version__ = version("quaternions")
+try:
+    __version__ = version("quaternions")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # for when running tests or in development
