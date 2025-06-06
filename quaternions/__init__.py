4c747d071d63b1bec9ea83c1c6a32a15153811d2
Giorgio Salluzzo <giorgio.salluzzo@satellogic.com>
2025-06-06 18:44:39 +0200

    Fix.

--- quaternions/__init__.py
+++ quaternions/__init__.py
@@ -1,5 +1,13 @@
+import warnings
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
 from quaternions.quaternion import Quaternion  # NOQA
 from quaternions.general_quaternion import GeneralQuaternion, QuaternionError  # NOQA
 
-import warnings
 warnings.simplefilter('once')
+
+__version__ = version("quaternions")
